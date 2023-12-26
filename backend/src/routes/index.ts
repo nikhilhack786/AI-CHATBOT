b804132538a99da139a5fe075c45825ab562bdc5
nikhilhack786 <nikhilg12456@gmail.com>
2023-12-26 15:14:57 +0530

    final project

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import userRoutes from "./user-routes.js";
+
+const appRouter = Router();
+
+appRouter.use("/user", userRoutes); //domain/api/v1/user
+appRouter.use("/chats", userRoutes); //domain/api/v1/chats
+
+
+export default appRouter;
